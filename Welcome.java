074e20df6f59f07cf76ed0b9565aad7c4d62cc3c
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-12-13 22:35:35 +0530

    13-12-2023

---
+++ Welcome.java
@@ -0,0 +1,17 @@
+class Welcome
+{
+		//starting point of java application
+	public static void main(String args[])
+	{
+		System.out.println("welcome to java ");
+		
+	}
+	
+}	//JVM-->binary 0's 1's byte code   translator --->translation
+
+//source /user understandable language   ---->machine /byte/binary  -->compiler-->compailation
+//			compiler-->compailation			execution/JVM
+// .java  -------------------------->.class------------->output
+		//	javac filename.java				java classname		
+
+

9572d4e474be5eb316d18f40898dfc96eab2499e
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-12-14 22:51:22 +0530

    Day-3 14-12-2023

---
+++ Variables_Ex.java
@@ -0,0 +1,20 @@
+class Variables_Ex
+{
+	int marks=2372923874;//global variable
+		//starting point of java application
+	public static void main(String args[])
+	{
+		//local variables
+		byte age=32;
+		long contact=9494943499l;
+		float salary=23.432429234916292f;
+		double salary1=23.432429234916292;
+		char gender='M';
+		boolean result=true;
+		
+				System.out.println(age+" "+contact+" "+salary+" "+salary1+" "+gender+" "+result);
+
+		
+	}
+	
+}

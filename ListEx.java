379aa5d25d4355abf9323d178edf68c77f00399d
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-02-01 22:30:08 +0530

    01-02-2024

--- ListEx.java
+++ ListEx.java
@@ -1,7 +1,7 @@
 package com.ot9.collections;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Iterator;
 
 //Collection:
 //1)collections will allow both homogeneous data and heterogenous is allowed
@@ -22,23 +22,38 @@ public class ListEx {
 		list.add("somesh");
 		list.add("suresh");
 		list.add(2, "sandeep");
-		System.out.println(list.contains("suresh"));
-		System.out.println(list.size());
+
 		System.out.println(list);
-		list.remove("sandeep");
-		System.out.println(list);
-		// list.clear();
-		System.out.println(list);
-		ArrayList<String> list1 = new ArrayList<String>();// generic -->10-->16 -->(cc*3/2)+1
-		list1.add("capgemini");
-		list1.add("cognizant");
-		System.out.println(list1);
-		list1.addAll(list);
-		System.out.println(list1);
-		// list1.removeAll(list);
-		// list1.retainAll(list);
-		System.out.println(list1.containsAll(list));
-		System.out.println(list1.isEmpty());
+
+		Iterator<String> itr = list.iterator();
+
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+		Object[] array = list.toArray();
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+
+//		System.out.println(list.contains("suresh"));
+//		System.out.println(list.size());
+//		System.out.println(list);
+//		list.remove("sandeep");
+//		System.out.println(list);
+//		// list.clear();
+//		System.out.println(list);
+//		ArrayList<String> list1 = new ArrayList<String>();// generic -->10-->16 -->(cc*3/2)+1
+//		list1.add("capgemini");
+//		list1.add("cognizant");
+//		System.out.println(list1);
+//		list1.addAll(list);
+//		System.out.println(list1);
+//		// list1.removeAll(list);
+//		// list1.retainAll(list);
+//		System.out.println(list1.containsAll(list));
+//		System.out.println(list1.isEmpty());
+
 	}
 
 }
--- MapEx.java
+++ MapEx.java
@@ -1,18 +1,57 @@
 package com.ot9.collections;
 
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class MapEx {
 
 	public static void main(String[] args) {
-		TreeMap<Integer,String>emps=new TreeMap<Integer,String>();//16
-					emps.put(123, "sandeep");//entry
-					emps.put(23, "suresh");
-					emps.put(14, "kalyan");
-					emps.put(5, "somesh");
-					emps.put(64, "rajesh");
-					emps.put(123, "mahesh");
-					System.out.println(emps);
+		HashMap<Integer, String> emps = new HashMap<Integer, String>();// 16
+		emps.put(123, "sandeep");// entry
+		emps.put(23, "suresh");
+		emps.put(14, "kalyan");
+		emps.put(5, "somesh");
+		emps.put(64, "rajesh");
+		emps.put(123, "mahesh");
+		Collection<String> al =emps.values();
+		for(String name:al)
+		{
+			System.out.println(name);
+		}
+		
+//		Set<Integer> keys = emps.keySet();
+//
+//		Iterator<Integer> itr = keys.iterator();
+//
+//		while (itr.hasNext()) {
+//			int key = itr.next();
+//			System.out.println(emps.get(key));
+//		}
+
+		Set<Entry<Integer, String>> keys = emps.entrySet();
+
+		Iterator<Entry<Integer, String>> itr = keys.iterator();
+
+		while (itr.hasNext()) {
+			Entry<Integer, String> entry = itr.next();
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+
+		// emps.remove(123);
+		System.out.println(emps);
+		System.out.println(emps.containsKey(1231));
+		System.out.println(emps.containsValue("mahesh"));
+		System.out.println(emps);
+		System.out.println(emps.get(64));
+		HashMap<Integer, String> emps1 = new HashMap<Integer, String>();// 16
+		emps1.put(143, "navya");
+		emps1.putAll(emps);
+		System.out.println(emps1);
+
 	}
 
 }
